2045fb8a3d607a87de02dddc41523198f4f9342a
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-11-22 22:06:08 +0100

    Complete todo by clic

--- app/Todo.jsx
+++ app/Todo.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import './Todo.scss';
 
 const Todo = React.createClass({
 
   render(){
     return(
-      <li style={{textDecoration: this.props.todo.completed ? 'line-through' : ''}}>
-        {this.props.todo.text} <input type="checkbox" value={this.props.todo.completed} onChange={this.props.handlerComplete} />
+      <li
+        className="Todo"
+        style={{textDecoration: this.props.todo.completed ? 'line-through' : ''}}
+        onClick={this.props.handlerComplete}
+      >
+        {this.props.todo.text} <input type="button" value="X" />
       </li>
-    )
+    );
   }
 });
 
