4dff957b732e22c8cabf07243cc569668ba142e7
Romain Durand Saint Omer <romain.dso@gmail.com>
2015-12-07 21:57:12 +0100

    Add some styles

--- app/Todo.jsx
+++ app/Todo.jsx
@@ -5,12 +5,12 @@ const Todo = React.createClass({
 
   render(){
     return(
-      <li
-        className="Todo"
-        style={{textDecoration: this.props.todo.completed ? 'line-through' : ''}}
-      >
+      <li className="Todo">
         <input type="checkbox" checked={this.props.todo.completed} onClick={this.props.handlerComplete}/>
-        <span onClick={this.props.handlerComplete}>{this.props.todo.text}</span>
+        <span
+          style={{textDecoration: this.props.todo.completed ? 'line-through' : ''}}
+          onClick={this.props.handlerComplete}>{this.props.todo.text}
+        </span>
         <input type="button" value="X" onClick={this.props.handlerDelete}/>
       </li>
     );
